cc1d540bc2566fb7e706abfd08d84186c3527892
dumodem <dustellame@yahoo.com>
2016-09-05 23:13:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def makeWebhookResult(req):
     username = parameters.get("user")
     userlikes = parameters.get("likes")
 
-    speech = "glad to know " + username + " likes " + userlikes
+    speech = parameters.get("botresponse")
     
     
     print("Response:")
