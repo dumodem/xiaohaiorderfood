3e7e048ef57872da5865ca8223f66ef4d3eb8c8e
dumodem <dustellame@yahoo.com>
2016-09-05 15:45:57 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "get-newproduct-for-username":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    username = parameters.get("username")
+    newproduct = parameters.get("newproduct")
+    userlikes = '香辣鸡翅'
+
+    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+
+    speech = "你好，" + username + "我知道你很喜欢吃" + userlikes + "。现在我们推出" + newproduct + "，味道够辣量又足，要不要试一试？"
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        #"data": {},
+        # "contextOut": [],
+        "source": "xiaohaiorderfood"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+    app.run(debug=True, port=port, host='0.0.0.0')
